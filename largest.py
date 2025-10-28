1d65191af19b719417ce9c64132bcd9af800563d
Tejaswini-puvvada <theyzpuvvada501@gmail.com>
2025-10-28 15:40:55 +0530

    Deleted unwanted files

--- largest.py
+++
@@ -1,8 +0,0 @@
-num=2589
-largest=0
-while num>0:
-    digit=num%10
-    if digit>largest:
-        largest=digit
-    num=num//10
-print(largest)
--- vowels.py
+++
@@ -1,7 +0,0 @@
-text="apple"
-vowel="aeiouAEIOU"
-count=0
-for char in text:
-    if char in vowel:
-        count=count+1
-print(count)
